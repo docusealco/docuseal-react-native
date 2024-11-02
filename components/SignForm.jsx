8e50d381e0958d307506cd23b6d79a508d097629
Alex Turchyn <alex@docuseal.com>
2024-11-02 16:00:56 +0200

    update domain

--- components/SignForm.jsx
+++ components/SignForm.jsx
@@ -10,10 +10,10 @@ export default function SignForm({ route }) {
         <html>
           <head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <script src="https://cdn.docuseal.co/js/form.js"></script>
+            <script src="https://cdn.docuseal.com/js/form.js"></script>
           </head>
           <body>
-            <docuseal-form style="height: 100vh" data-src="https://docuseal.co/d/${slug}" data-email="${email}">
+            <docuseal-form style="height: 100vh" data-src="https://docuseal.com/d/${slug}" data-email="${email}">
               Loading...
             </docuseal-form>
           </body>
